33a195c0f42df8bb2a93e1c75e4199558328b110
ilias-chergui <chergui.ilias@gmail.com>
2017-10-26 09:31:41 +0200

    upload exo 4

---
+++ 04-Boucles/app.js
@@ -0,0 +1,33 @@
+//Exo 1  Pair - Impair
+/*
+  for (var i = 0; i <= 20; i++)
+    {if (i%2 == 0)
+      {console.log(i + " est pair")}
+      else {console.log(i + " est impair")}
+    }
+*/
+
+//Exo 2 Multiple table
+/*
+  for (var i = 0; i <= 10; i++)
+    {console.log(i + " * 9 = " + i * 9)}
+*/
+
+//Exo 3 Assign grades
+/*
+  for (var score = 0; score <=100; score++)
+  {if (score > 90) {console.log("Pour " + score + " points vous avez le grade A")}
+    else if (score <= 90 && score > 80) {console.log("Pour " + score + " points vous avez le grade B")}
+      else if (score <= 80 && score > 70) {console.log("Pour " + score + " points vous avez le grade C")}
+        else if (score <= 70 && score > 65) {console.log("Pour " + score + " points vous avez le grade D")}
+        else {console.log("Pour " + score + " points vous avez le grade F")}
+  }
+*/
+
+//Exo 4 Pyramide
+/*
+  for (var a = 1; a <= 5; a++)
+      { for (var b = 0; b < a; b++)
+          { document.write("*") }
+        document.write("<br/>") }
+*/
