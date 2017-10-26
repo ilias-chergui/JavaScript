a0ccc678bdb69c0872296566163e69100d8a2469
ilias-chergui <chergui.ilias@gmail.com>
2017-10-26 09:32:44 +0200

    upload exo 7

---
+++ 07-Maths/app.js
@@ -0,0 +1,43 @@
+//Exo 1 Mini-MAxi
+/*
+  var petit = Math.min(7, 5, -12, 6, 32, 18, 14, -2);
+  console.log(petit);
+
+  var grand = Math.max(-3, 9, 21, 36, 27, 54, 17, 35);
+  console.log(grand);
+
+  var addition = grand + petit;
+  console.log("L'addition de ces nombres vaut " + addition);
+*/
+
+//Exo 2 Aleatoire 1°
+/*
+  var floatBateau = 10.4;
+  console.log("Il vaut " + floatBateau);
+
+  var voleBateau = Math.ceil(floatBateau);
+  console.log("Arrondir au-dessus = " + voleBateau);
+
+  var couleBateau = Math.floor(floatBateau);
+  console.log("Arrondir en-dessous = " + couleBateau);
+*/
+
+//Exo 3 Aleatoire 2°
+/*
+  var mini = Math.floor(50);
+  var maxi = Math.ceil(100);
+  console.log(Math.floor(Math.random() * (maxi - mini) +50));
+
+  var one = Math.random();
+  console.log(Math.round(one));
+
+  var ten = Math.floor(Math.random() * 11);
+  console.log(ten);
+*/
+
+//Exo 4 Aleatoire 3°
+/*
+  var personnages = ["Goro", "Johnny Cago", "Kano", "Liu Kano", "Raiden", "Reptil", "Scorpion", "Shang Tsun", "Sonya", "Sub-Zero"];
+  var random = personnages[Math.floor(personnages.length * Math.random())]
+  console.log(random);
+*/
