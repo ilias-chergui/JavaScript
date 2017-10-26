efe4ff7300ab4dd3300357781f97a6c444317110
ilias-chergui <chergui.ilias@gmail.com>
2017-10-26 09:31:19 +0200

    upload exo 3

---
+++ 03-Fonctions/app.js
@@ -0,0 +1,19 @@
+//Exo 1 Calculs
+/*
+function soustraction( a , b )
+  { return a - b }
+function division( c , d )
+  { return c / d }
+function multiplication( e , f )
+  { return e * f }
+function pourcentage( nombre , pourcent )
+  { return nombre * pourcent / 100 }
+function vitesse( km , heure )
+  { return km / heure }
+
+console.log("5 - 2 = " + soustraction( 5 , 2 ))
+console.log("20 / 10 = " + division( 20 , 10 ));
+console.log("5 * 3 = " + multiplication( 5 , 3 ));
+console.log("50% de 60 = " + pourcentage( 60 , 50 ));
+console.log("Si je parcours 240km en 2h, ma vitesse est égale à " + vitesse( 240 , 2 ) + " km/h");
+*/
