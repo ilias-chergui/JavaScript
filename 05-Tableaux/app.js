d79bcfa225826d7dfc6421de4737d78efd17d3b1
ilias-chergui <chergui.ilias@gmail.com>
2017-10-26 09:32:09 +0200

    upload exo 5

---
+++ 05-Tableaux/app.js
@@ -0,0 +1,34 @@
+//Exo 1 Addition
+/*
+  var chiffre = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  console.log(chiffre);
+
+  var resultat = 0;
+  for (var i = 0; i < chiffre.length; i++)
+    { resultat = resultat + chiffre[i]; }
+*/
+
+//Exo 2 Choix
+/*
+  var acteur = ["Stalone", "Cruiz", "Freeman"];
+
+  for (var i = 1; i < acteur.length+1; i++)
+  { console.log("Le numero " + i + " est " + acteur[i-1]); }
+
+  //BONUS
+    var acteur = ["Stalone", "Cruiz", "Freeman"];
+    var classement = ["premier ", "deuxieme ", "troisieme "];
+
+    for (var i = 0; i < acteur.length; i++)
+      { resultat = "Le " + classement[i] + "est " + acteur[i];
+        console.log(resultat); }
+*/
+
+//Exo 3 Clone
+/*
+  var perso = ["Mario", "Luigi", "Peach"];
+  var double = perso.slice(0);
+  double.push("Bowser");
+
+  console.log(double);
+*/
