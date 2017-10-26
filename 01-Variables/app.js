173de39b48ea8f64d7dc18b0bd2557926e577f5e
ilias-chergui <chergui.ilias@gmail.com>
2017-10-26 09:27:53 +0200

    upload exo 1

---
+++ 01-Variables/app.js
@@ -0,0 +1,40 @@
+// Exo 1
+/*
+  var enfants = 1;
+  var femme = "Amandine";
+  var pays = "Belgique";
+  var métier = "Développeur";
+
+  console.log(" Vous serez " + métier + " et habiterez à " + pays + " , et marié à " + femme + " avec " + enfants + " enfants ");
+*/
+
+//Exo 2
+/*
+  var année = 2017;
+  var naissance = 1992;
+
+  console.log("Il est agé de " + (année - naissance) + " ans.");
+*/
+
+//Exo 3
+/*
+  var monage = 25;
+  var agemax = 100;
+  var aliment = "pommes";
+  var parjour = 3;
+
+  console.log("Il vous reste à manger " + ((agemax - monage) * parjour * 365) + " fois des " + aliment + " avant d'ateindre l'age de " + agemax + " ans.");
+*/
+
+//Exo 4
+/*
+  var etape1 = 1 + 2;
+  var etape2 = (1 + 2) * 3;
+  var etape3 = 4 / 2;
+  var etape4 = (1 + 2) * 3 + 4 / 2;
+
+  console.log("Étape 1 = " + etape1 + " * 3 + 4 / 2");
+  console.log("Étape 2 = " + etape2 + " + 4 / 2");
+  console.log("Étape 3 = 9 + " + etape3);
+  console.log("Étape 4 = " + etape4);
+*/
